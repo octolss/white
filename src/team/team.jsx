60cbf2f4448691083fab9a57ca46349fe6db5cd3
Artemijs Belokrilecs <artjombelich@gmail.com>
2024-03-29 23:02:37 +0300

    Team

--- src/team/team.jsx
+++ src/team/team.jsx
@@ -8,24 +8,32 @@ function Team() {
             <div className="firstRow">
                 <div className="rectangle">
                     <img src = {vovka}/>
+                    <p>Vovka Jeremmev</p>
+                    <p>CO-CEO/Full-Stack</p>
                 </div>
                 <div className="rectangle">
                 <img src = {vovka}/>
+                <p>Goblin Goblin</p>
+                    <p>Goblin Goblin</p>
                 </div>
             </div>
             <div className="secondRow">
                 <div className="rectangle">
                 <img src = {vovka}/>
+                    <p>Artemka Belij</p>
+                    <p>CO-CEO/Full-Stack</p>
                 </div>
                 <div className="rectangle">
                 <img src = {vovka}/>
+                <p>Incognito</p>
+                    <p>Web-Design/UI-UX</p>
                 </div>
             </div>
                 
         </div>
         <div className="aboutTeam">
             <h1>Our Team:</h1>
-
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores cupiditate consequuntur ipsa iure aspernatur cumque numquam veniam molestias quaerat.</p>
             <button className='showAbout'>Show more...</button>
             
         </div>
